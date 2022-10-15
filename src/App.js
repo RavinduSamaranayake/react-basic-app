eaa48d4976d601936036aa70b6ea5ed3707f9209
Ravindu95 <kushan.r@ustocktrade.com>
2022-10-15 20:37:31 +0530

    completed input handler

--- src/App.js
+++ src/App.js
@@ -2,32 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Clock from './components/Clock'
-import UserProfile,{UserRole} from './components/UserProfile';
 
 function App() {
-
-  const [title,setTitle] = React.useState('Hello.....');//we can add number 5 also here. so we have to give a type here. for that let's use react props types
   
-  const onSubmitCalled = (name,age) => {
-    console.log('Submit Event Called in Parent Component : ',name,age);
+  const handleInputChange = (event,name,age,obj) => {
+    console.log(event.target.value,name,age,obj);
   }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the Site</h1> 
-        <Clock/>
-        <UserProfile
-         title = {title}
-         name = 'Kushan'
-         address= {{
-          zip : '1234',
-          city : 'Elp'
-         }}
-         onSubmitCalledInParent = {onSubmitCalled}
-         /> 
-        {UserRole.ADMIN}
+        {/* <Clock/> */}
+        <input type="text" onChange={(event) => handleInputChange(event,"Kushan",27,{})}></input>
       </header>
     </div>
   );
