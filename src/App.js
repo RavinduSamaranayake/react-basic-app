72e499b439d61276461178d30879c53942b0bf97
Ravindu95 <kushan.r@ustocktrade.com>
2022-10-16 08:11:41 +0530

    Added Key value list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ function App() {
     console.log(event.target.value,name,age,obj);
   }
 
+  const items = [
+    {id: 1,value: 11},
+    {id: 2,value: 12},
+    {id: 3,value: 13},
+    {id: 4,value: 111},
+    {id: 5,value: 210},
+    {id: 6,value: 701},
+    {id: 7,value: 191} ]
+   
+  const Item = (props) => {
+    return <li>{props.value}</li>
+  }  
+
 
   return (
     <div className="App">
@@ -17,6 +30,9 @@ function App() {
         <h1>Welcome to the Site</h1> 
         {/* <Clock/> */}
         <input type="text" onChange={(event) => handleInputChange(event,"Kushan",27,{})}></input>
+        <ul>
+          {items.map(num => <Item key={num.id} value={num.value}/>)}
+        </ul>
       </header>
     </div>
   );
