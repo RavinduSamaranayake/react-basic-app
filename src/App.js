85214db1cd0950920ab5a054a2e7ae3e3b7b484e
Ravindu95 <kushan.r@ustocktrade.com>
2022-10-17 10:00:29 +0530

    completed useref with useImperativeHandle

--- src/App.js
+++ src/App.js
@@ -1,67 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 
 function App() {
 
-  const element1 = <h3>Hello Kushan</h3> //element create using JSX (JavaScript XML)-> EASSY WAY
-  const element2 = React.createElement('h3',null,'Hello Ravindu') // above JSX converts to this type
+  const parentRef = useRef(null);
 
-  const element3 = <div>
-    <div>Are you sure want to do this ?</div>
-  </div>
+  const handleClick = () => {
+    parentRef.current.clickMe();
+  }
 
-  const element4 = React.createElement('div',null,React
-  .createElement('div',null,'Are you sure want to do this2s ?'))
 
-  const SampleComp = (props) => {
-    let color = ''
-    switch(props.type){
-      case 'SUBMIT':
-        color = 'blue'
-        break
-      case 'CANCEL':
-        color = 'red'
-        break
-      case 'CONFIRM':
-        color = 'green'
-        break
-      default:
-        color = 'white'
-    }
-    return (
-      <button style = {{background: color}}>{props.text}</button>
-     ) 
-   }
+  let MyInput = (props,ref) => {
+    const inputRef = useRef();
+    useImperativeHandle(ref, () => ({
+      clickMe : () => {
+        console.log('Click Me');
+        inputRef.current.focus();
+      }
+    }));
+    return <input ref={inputRef}/>
+  }
 
-   const SampleComp2 = () => {
-    return (<div>
-      <p>{element3}</p>
-      <p>{element4}</p>
-    </div>) 
-   }
+  MyInput = forwardRef(MyInput);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the Site</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-          {/* elements */}
-          <p>{element1}</p>
-          <p>{element2}</p>
-
-        {/* components */}
-        <SampleComp type='SUBMIT' text='Submit'/>
-        <SampleComp type='CANCEL' text='Cancel'/>
-        <SampleComp type='CONFIRM' text='Confirm'/>
-
-        <SampleComp2/>   
-
-  
+        <MyInput ref = {parentRef}/>
+        <br/>
+        <button onClick={handleClick}>Click</button>
       </header>
     </div>
   );
